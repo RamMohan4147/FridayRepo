345a89da58191118bf4b695ae54c7f06794dce08
Ram Mohan Kanchi <102355266+RamMohan4147@users.noreply.github.com>
2024-06-21 10:46:41 +0530

    qwewqewqewq
    wqeqweqwqwe

---
+++ Test.java
@@ -0,0 +1,13 @@
+import btech.*;
+class Test
+{
+	public static void main(String args[])
+	{
+		Cse c=new Cse();
+		c.inr();
+		Ece e=new Ece();
+		e.ramesh();
+		Eee e1=new Eee();
+		e1.kumar();
+	}
+}
